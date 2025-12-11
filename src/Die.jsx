5744378f0c2278a3437f61e6c342f42af89a6d63
Ana-Marija Drašković <ana.marija.draskovic007@gmail.com>
2025-12-11 16:41:23 +0100

    Render Die component with dots instead of numbers

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,13 +1,35 @@
 import "./app.css";
 
+const dotPatterns = {
+  1: [4],
+  2: [0, 8],
+  3: [0, 4, 8],
+  4: [0, 2, 6, 8],
+  5: [0, 2, 4, 6, 8],
+  6: [0, 2, 3, 5, 6, 8],
+};
+
 export function Die({ value, isHeld, holdDie }) {
   const styles = {
     backgroundColor: isHeld ? "#59E391" : "white",
   };
 
   return (
-    <button type="button" className="die" style={styles} onClick={holdDie}>
-      {value}
+    <button
+      type="button"
+      className="die"
+      style={styles}
+      onClick={holdDie}
+      aria-label={value}
+    >
+      <div className="dots">
+        {Array.from({ length: 9 }, (_, idx) => (
+          <span
+            key={`${value} - ${idx}`}
+            className={`dot ${dotPatterns[value].includes(idx) ? "filled" : ""}`}
+          />
+        ))}
+      </div>
     </button>
   );
 }
